88105bc8ab893409970601ff2b9114b3a210c6c0
Volodymyr Hlushko <volodymyr@refactor.digital>
2023-07-20 10:04:12 +0100

    delete all unnecessary code

---
+++ dev/src/global.js
@@ -0,0 +1,10 @@
+require('./scripts/constants.js');
+require('./scripts/pubsub.js');
+
+function debounce(fn, wait) {
+	let t;
+	return (...args) => {
+		clearTimeout(t);
+		t = setTimeout(() => fn.apply(this, args), wait);
+	};
+}
